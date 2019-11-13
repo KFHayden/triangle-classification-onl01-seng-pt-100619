7feda303b4f9374508e2d8bade5b4f9e6c6317e1
Kevin Hayden <kevin.fhayden@gmail.com>
2019-11-13 19:34:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,21 @@
 class Triangle
 
   def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+    a = a
+    b = b
+    c = c
   end
   
   def kind
-    if @a <= 0 || @b <= 0 || @c <= 0
+    if a <= 0 || b <= 0 || c <= 0
       raise TriangleError
-    elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+    elsif a + b <= c || b + c <= a || a + c <= b
       raise TriangleError
     end
       
-    if @a == @b && @b == @c
+    if a == b && b == c
       :equilateral
-    elsif (@a != @b) && (@b != @c) && (@a != @c)
+    elsif (a != b) && (b != c) && (a != c)
       :scalene
     else
       :isosceles
